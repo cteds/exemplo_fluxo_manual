248c8405aae50853e92d0a7f866316d4c1b3df1f
saulomsantos <saulo.macedoprojetos@gmail.com>
2022-09-12 18:02:25 -0300

    added - endpoint to list studios

---
+++ Games/Controllers/StudiosController.cs
@@ -0,0 +1,25 @@
+﻿using Games.Interfaces;
+using Games.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Games.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudiosController : ControllerBase
+    {
+        private IStudio StudioRepository { get; set; }
+
+        public StudiosController()
+        {
+            StudioRepository = new StudioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok(StudioRepository.List());
+        }
+    }
+}
